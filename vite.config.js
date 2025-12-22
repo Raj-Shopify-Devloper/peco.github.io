2e66992b1891bf82d692095f0cbadfcbc4373ece
Raj-webdesigner <raj.s.webdevloper@gmail.com>
2025-12-22 11:23:17 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/peco.github.io/',
   plugins: [react()],
   optimizeDeps: {
     include: ['react-slick > slick-carousel']  // Forces Vite to pre-bundle slick-carousel properly
